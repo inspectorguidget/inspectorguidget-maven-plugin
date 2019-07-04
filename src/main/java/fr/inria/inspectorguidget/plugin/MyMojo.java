63d62046e56eb24b9befb3756d6c3af77ac88733
Ludovic Richoux <ludovic.richoux@inria.fr>
2019-07-04 14:40:18 +0200

    update(Mojo) launch commandAnalyser with the project directory

--- src/main/java/fr/inria/inspectorguidget/plugin/MyMojo.java
+++ src/main/java/fr/inria/inspectorguidget/plugin/MyMojo.java
@@ -1,31 +1,24 @@
 package fr.inria.inspectorguidget.plugin;
 
 
+import fr.inria.inspectorguidget.api.analyser.CommandAnalyser;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
-import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
-/**
- * Goal which touches a timestamp file.
- */
-@Mojo( name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
-public class MyMojo
-    extends AbstractMojo
-{
-    /**
-     * Location of the file.
-     */
-    @Parameter( defaultValue = "${project.build.directory}", property = "outputDir", required = true )
-    private File outputDirectory;
+@Mojo( name = "extractdata" )
+public class MyMojo extends AbstractMojo {
+
+    @Parameter( defaultValue = "${project}", required = true, readonly = true)
+    private File projectDirectory;
 
     public void execute() throws MojoExecutionException {
-        getLog().info("Hello World !");
+        CommandAnalyser analyser = new CommandAnalyser();
+        analyser.addInputResource(projectDirectory.getAbsolutePath());
+        analyser.run();
     }
 }
