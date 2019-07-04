d80f64fb6d99b2091fd0937578cd89f634a74d89
Ludovic Richoux <ludovic.richoux@inria.fr>
2019-07-04 11:37:06 +0200

    new(plugin) building plugin structure

---
+++ src/main/java/fr/inria/inspectorguidget/plugin/MyMojo.java
@@ -0,0 +1,31 @@
+package fr.inria.inspectorguidget.plugin;
+
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Goal which touches a timestamp file.
+ */
+@Mojo( name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
+public class MyMojo
+    extends AbstractMojo
+{
+    /**
+     * Location of the file.
+     */
+    @Parameter( defaultValue = "${project.build.directory}", property = "outputDir", required = true )
+    private File outputDirectory;
+
+    public void execute() throws MojoExecutionException {
+        getLog().info("Hello World !");
+    }
+}
