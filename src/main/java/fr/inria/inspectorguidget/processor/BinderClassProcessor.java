37dc57f458e4d2eb245ee32adde6a161b8a9366b
Ludovic Richoux <ludovic.richoux@inria.fr>
2019-08-22 09:33:10 +0200

    update(processor) detecting class with binders

--- src/main/java/fr/inria/inspectorguidget/processor/BinderClassProcessor.java
+++ src/main/java/fr/inria/inspectorguidget/processor/BinderClassProcessor.java
@@ -1,7 +1,11 @@
 package fr.inria.inspectorguidget.processor;
 
 import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtInvocation;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtElement;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.visitor.filter.AbstractFilter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +16,21 @@ public class BinderClassProcessor extends AbstractProcessor<CtClass> {
 
     @Override
     public boolean isToBeProcessed(CtClass candidate){
+
+        try{
+            CtInvocation invoc = candidate.getElements(new AbstractFilter<CtInvocation>() {
+                @Override
+                public boolean matches(CtInvocation invocation) {
+                    if(invocation.toString().endsWith("bind()"))
+                        return true;
+
+                    return false;
+                }
+            }).get(0);
+        } catch (Exception e){
+            return false;
+        }
+
         return true;
     }
 
